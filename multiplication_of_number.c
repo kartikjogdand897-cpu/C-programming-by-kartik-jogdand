64e0a154a93ee6509a072c85d9a9a42121b91c47
kartikjogdand897-cpu <kartikjogdand897@gmail.com>
2025-11-07 23:46:09 +0530

    multiplication

---
+++ multiplication_of_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
+
+
+    printf("mul is: %d", a * b);
+    return 0;
+}
